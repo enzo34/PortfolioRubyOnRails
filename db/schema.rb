9c32bef408f55b0716fa7e2b530a10efdaa9addd
enzo34 <eclavelloux@gmail.com>
2018-07-09 10:15:38 +0200

    Integration of the skills

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_151224) do
+ActiveRecord::Schema.define(version: 2018_07_09_080140) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_use"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
