10ad433b91d535fd3c4647e0e547bc9077ce9636
enzo34 <eclavelloux@gmail.com>
2018-07-10 09:24:12 +0200

    Implementation of the edit feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
     
     def index
         @portfolio_items = Portfolio.all
@@ -20,9 +21,26 @@ class PortfoliosController < ApplicationController
     end
   end
 
+   def edit
+      set_portfolio
+   end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Le Portfolio Item a été mis à jour' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
 private
     def portfolio_params
         params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    end
+
+    def set_portfolio
+       @portfolio_item = Portfolio.find(params[:id]) 
     end
 end
