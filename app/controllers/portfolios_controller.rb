7e0395d51e629de4ad67bf8b86de36289c58e77a
enzo34 <eclavelloux@gmail.com>
2018-07-19 12:16:49 +0200

    Add partials for form and bar bug on the delete function of the portfolio signal

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -51,11 +51,17 @@ class PortfoliosController < ApplicationController
       @seo_keywords = @portfolio_item.body
   end
 
-  def destroy
-      @portfolio_item.destroy
-      respond_to do |format|
-          format.html { redirect_to portfolios_url, notice: 'Le portfolio item a été supprimer'}
-      end
+ def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
 
 private
