4f85f395b54781372fb6f2434f306f6a3914c656
enzo34 <eclavelloux@gmail.com>
2018-07-23 09:59:35 +0200

    Add styles for application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
     def login_helper
         if current_user.is_a?(GuestUser)
-         (link_to "Inscription", new_user_registration_path) + 
-         ("<br />".html_safe) +
-         (link_to "Se connecter", new_user_session_path)
+         (link_to "Inscription", new_user_registration_path, class:"nav-link") +
+         (link_to "Se connecter", new_user_session_path, class:"nav-link")
         else
-         (link_to "Editer votre profil", edit_user_registration_path) +
-         ("<br />".html_safe) +
-         (link_to "Deconnecter", destroy_user_session_path, method: :delete)
+         (link_to "Editer votre profil", edit_user_registration_path, class:"nav-link") +
+         (link_to "Deconnecter", destroy_user_session_path, method: :delete, class:"nav-link")
         end 
     end
     
