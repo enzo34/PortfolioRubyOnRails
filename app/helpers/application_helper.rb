42ef43fbc5839622a36c3f96d64588eaf533a8af
enzo34 <eclavelloux@gmail.com>
2018-07-23 14:24:53 +0200

    Update master layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-    def login_helper
+    def login_helper style
         if current_user.is_a?(GuestUser)
-         (link_to "Inscription", new_user_registration_path, class:"nav-link") +
-         (link_to "Se connecter", new_user_session_path, class:"nav-link")
+         (link_to "Inscription", new_user_registration_path, class:style) +
+         (link_to "Se connecter", new_user_session_path, class:style)
         else
-         (link_to "Editer votre profil", edit_user_registration_path, class:"nav-link") +
-         (link_to "Deconnecter", destroy_user_session_path, method: :delete, class:"nav-link")
+         (link_to "Editer votre profil", edit_user_registration_path, class:style) +
+         (link_to "Deconnecter", destroy_user_session_path, method: :delete, class:style)
         end 
     end
     
