5b4230bcda8620eab6a9fec4b47806901020cc8c
enzo34 <eclavelloux@gmail.com>
2018-07-28 13:17:07 +0200

    refactoring nav, debugging, implemente jquery (finally), making helper, final implementations for the style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,42 @@ module ApplicationHelper
             content_tag(:p, content, class: "source_content")
         end  
     end
+
+    def nav_items
+        [
+            {
+              url: root_path,
+              title: 'Accueil'
+             },
+            {
+              url: about_me_path,
+              title: 'En savoir +'
+             },
+            {
+                url: contact_path,
+                title: 'Nous contacter'
+            },
+            {
+                url: blogs_path,
+                title: 'Blog'
+            },
+            {
+                url: portfolios_path,
+                title: 'Projects'
+            },
+
+        ]
+    end
+
+    def nav_helper style, tag_type
+        nav_links = ''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+        nav_links.html_safe
+    end
+
+    def active? path
+        "active" if current_page? path
+    end
 end
