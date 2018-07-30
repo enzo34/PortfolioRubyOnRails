d8e53cba1a7f18f2f565cf9f17abc9f6ab75a637
enzo34 <eclavelloux@gmail.com>
2018-07-30 17:32:58 +0200

    Refactor form style for upload and refactor placholder with a helper

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,5 @@
 class Skill < ApplicationRecord
     include Placeholder
     validates_presence_of :title, :percent_use
-    
-    after_initialize :set_defaults
-    
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '300', width: '150')
-  end
 end
 
