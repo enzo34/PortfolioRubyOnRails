b22529128aa6e385622cd2f22d1f3cd5125f0a0a
enzo34 <eclavelloux@gmail.com>
2018-07-30 13:16:19 +0200

    Integrate image for the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,15 @@ class PagesController < ApplicationController
   def home
       @posts = Blog.all
       @skills = Skill.all
+      @page_title = "Home"
   end
 
   def about
+    @page_title = "A propos"
+
   end
 
   def contact
+    @page_title = "Contact"
   end
 end
