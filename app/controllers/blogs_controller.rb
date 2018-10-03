81114fe981d089add2a80a03029dce32e37a59a3
enzo34 <eclavelloux@gmail.com>
2018-10-03 14:47:53 +0200

    Fix Bug et ajout d'une validation avec Topic pour les blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     if logged_in?(:site_admin)
-      @blogs = Blog.recent.all
+      @blogs = Blog.recent.page(params[:page]).per(5)
     else
       @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
@@ -24,41 +24,41 @@ class BlogsController < ApplicationController
       @page_title = @blog.title
       @seo_keywords = @blog.body
     else
-      redirect_to blogs_path, notice: "Forbidden access"
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
+  end
 
-    # GET /blogs/new
-    def new
-      @blog = Blog.new
-    end
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
 
-    # GET /blogs/1/edit
-    def edit
-    end
+  # GET /blogs/1/edit
+  def edit
+  end
 
-    # POST /blogs
-    # POST /blogs.json
-    def create
-      @blog = Blog.new(blog_params)
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @blog = Blog.new(blog_params)
 
-      respond_to do |format|
-        if @blog.save
-          format.html { redirect_to @blog, notice: 'Your post is now live.' }
-        else
-          format.html { render :new }
-        end
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
-    # PATCH/PUT /blogs/1
-    # PATCH/PUT /blogs/1.json
-    def update
-      respond_to do |format|
-        if @blog.update(blog_params)
-          format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        else
-          format.html { render :edit }
-        end
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
@@ -84,13 +84,13 @@ class BlogsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_blog
-    @blog = Blog.friendly.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.friendly.find(params[:id])
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def blog_params
-    params.require(:blog).permit(:title, :body)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def blog_params
+      params.require(:blog).permit(:title, :body, :topic_id)
+    end
 end
